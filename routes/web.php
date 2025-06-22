6cdbcb33edfd5a8e91677fc926145009ecfd7016
Libe <mrlibelula@gmail.com>
2025-06-21 22:17:13 -0400

    trying to fix production 404

--- routes/web.php
+++ routes/web.php
@@ -36,7 +36,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return redirect()->route('login');
+    // return redirect()->route('login');
+    return view('auth.login');
 });
 
 Route::middleware([
